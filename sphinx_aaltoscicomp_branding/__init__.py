11d665dff3e7b2112d590dc394998ca854ecb315
Richard Darst <rkd@zgib.net>
2022-10-20 20:53:46 +0300

    sphinx_aaltoscicomp_branding/__init__: Fix verbosity some

--- sphinx_aaltoscicomp_branding/__init__.py
+++ sphinx_aaltoscicomp_branding/__init__.py
@@ -7,13 +7,13 @@ def setup(app):
 
     def config_hook(app, config):
         """Hook to set the HTML favicon"""
-        print('x'*500)
         config.html_favicon = abspath(join(dirname(__file__), '_static', 'logo-hexagons-02-compact.svg.ico'))
 
     # Only add the hook if we think we are being built in an AaltoSciComp
     # location:
     if (environ.get('GITHUB_REPOSITORY', '').lower().startswith('aaltoscicomp')
           or 'AALTOSCICOMP' in environ):
+        print("Enabling sphinx-aaltoscicomp-branding in this build.")
         app.connect('config-inited', config_hook)
 
     return {
